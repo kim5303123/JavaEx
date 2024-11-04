964a1153af15075be272c920311806191542be5e
kim5303123 <kim5303123@gmail.com>
2024-11-04 15:57:10 +0900

    do ~ while 문 , for 문

--- src/com/javaex/basic/flow/LoopEx.java
+++ src/com/javaex/basic/flow/LoopEx.java
@@ -21,7 +21,9 @@ public class LoopEx {
 		// TODO Auto-generated method stub
 
 //		whileEx(); 
-		whileGugu();
+//		whileGugu();
+//		doWhileEx();
+		forGugu();
 	}
 
 	private static void whileEx() {
@@ -50,7 +52,6 @@ public class LoopEx {
 		 System.out.print("단 : ");
 		 dan = sc.nextInt();
 		 while(i < 10){			 
-//			 System.out.println("단 :"+ dan );
 			 System.out.println( dan + "*" + i + "=" + (dan*i));
 			 i += 1 ;
 			 
@@ -59,7 +60,55 @@ public class LoopEx {
 		 
 	}
 		 
-	
+	private static void doWhileEx() {
+//		입력받은 정수를 다 더한다.
+//		입력 받은 정수가 0이면 반복하지 않는다.
+		
+		int total = 0;	//	합산 변수	
+		int value = 0;	//	더해주는 수, 반복제어 변수
+		
+		
+		System.out.println("숫자입력[ 0 to quit ] >");		
+		Scanner scanner = new Scanner(System.in);
+		
+//		최소 1번 실행		
+		do {	
+			value = scanner.nextInt();
+			total += value;
+			if( value != 0) {
+			System.out.println("합계 :" + total);
+			} else {
+				System.out.println("합계 :" + total);	
+				System.out.println("종료");
+			}
+		
+		} while(value != 0);
+		scanner.close();
+	}
 
+	private static void forGugu() {
+		
+//		단을 입력 받아서 해당 단의 구구표를 출력
+//		(for loop)
+//		for (반복변수 초기화; 반복 조건 확인; 반복변수 증감)
+		
+//		[문제]
+//		숫자를 입력받아 입력한 숫자(단)의
+//		구구단을 출력하세요 (for문으로작성)
+		
+		Scanner scanner = new Scanner(System.in);		
+		int dan;		 		
+		
+		System.out.println("단을 입력해주세요");
+		System.out.print("단 :");
+		dan = scanner.nextInt();
+		
+		for (int i = 1 ; i < 10 ; i++ ) {	//	반복 변수 초기화 ; 반복 조건 체크 ; 반복 변수 증감			
+			System.out.println(dan +"*" + i + "=" + dan*i );
+		}
+		scanner.close();
+		
+	}
+	
 	
 }
