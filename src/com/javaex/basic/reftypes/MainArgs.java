461df1a20bbc238710b40a65e29074befbac1ee1
kim5303123 <kim5303123@gmail.com>
2024-11-06 14:22:52 +0900

    MainArgs commmit

---
+++ src/com/javaex/basic/reftypes/MainArgs.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package com.javaex.basic.reftypes;
+
+import java.util.Arrays;
+
+/**
+ * @Author : 202-12
+ * @Date   : 2024. 11. 6.
+ */
+public class MainArgs {
+
+	/**
+	 * @Author : 202-12
+	 * @Date   : 2024. 11. 6.
+	 * @Method : main
+	 * @return : void
+	 */
+	public static void main(String[] args) {
+//		매개변수 목록이 args로 전달된다
+		System.out.println("전달된 매개 변수 : " + Arrays.toString(args));
+
+	}
+	
+}
