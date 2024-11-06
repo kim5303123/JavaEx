f73fd32ceb14de829f352bea339fce31d17d9e24
kim5303123 <kim5303123@gmail.com>
2024-11-06 10:17:59 +0900

    배열의 복사 commit

--- src/com/javaex/basic/reftypes/ArrayEx.java
+++ src/com/javaex/basic/reftypes/ArrayEx.java
@@ -18,8 +18,10 @@ public class ArrayEx {
 	 * @return : void
 	 */
 	public static void main(String[] args) {
-		usingArray();
+//		usingArray();
 //		multidimArrayEx();
+//		arrayCopyForEx();
+		arrayCopySystem();
 		
 	}
 
@@ -100,6 +102,45 @@ public class ArrayEx {
 			System.out.println();
 		}
 		System.out.println("합산 결과 :" + sum);
+	}
+	
+	private static void arrayCopyForEx() {
+		
+//		배열은 크기변경이 안된다.
+//		새 배열을 생성하고 복사한다.
+		
+		int source[] = { 1, 2, 3 };
+		System.out.println("원본 : " + Arrays.toString(source));
+		
+//		7개의 빈 공간이 더 필요
+		int target[] = new int[10];
+//		복사
+		for(int i = 0; i < source.length ; ++i) {
+			
+//			내용 복사
+			target[i] = source[i];
+			
+			System.out.println("복사본: "+ Arrays.toString(target));
+		}
+		
+		System.out.println(Arrays.toString(target));
+		
+	}
+
+	private static void arrayCopySystem() {
+		
+//		손쉬운 배열 복제
+		int[] source = { 1, 2, 3 };
+		System.out.println("원본 : " + Arrays.toString(source));
+		
+		int[] target = new int[10];
+		
+//		복제
+//		System.arraycopy(원본배열, 시작인덱스, 타켓배열, 타겟 배열의 시작 인덱스, 복사할 길이)
+		System.arraycopy(source, 0, target, 0, source.length);	 
+		
+		System.out.println("복사본 : "+ Arrays.toString(target));
+		
 	}
 	
 }
