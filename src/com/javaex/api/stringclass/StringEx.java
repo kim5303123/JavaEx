22cf0477c859154719c0a77a7a03054bb3175747
kim5303123 <kim5303123@gmail.com>
2024-11-11 15:27:25 +0900

    String 클래스의 메서드들 commit

--- src/com/javaex/api/stringclass/StringEx.java
+++ src/com/javaex/api/stringclass/StringEx.java
@@ -3,6 +3,8 @@
  */
 package com.javaex.api.stringclass;
 
+import java.util.Arrays;
+
 /**
  * @Author : 202-12
  * @Date   : 2024. 11. 11.
@@ -17,7 +19,8 @@ public class StringEx {
 	 */
 	public static void main(String[] args) {
 	
-		stringBasic();
+//		stringBasic();
+		usefulMethods();
 	}
 	
 	private static void stringBasic() {
@@ -42,7 +45,60 @@ public class StringEx {
 		String s5 = String.valueOf(PI);
 		System.out.println(s5);
 		
+	}
+	
+	private static void usefulMethods() {
+//		유용한 메서드들
+		String str = "Java Programming, JavaScript Programming";
 		
+//		문자열의 길이
+		System.out.println(str.length());
+		
+//		변환 메서드
+		System.out.println(str.toUpperCase());
+		System.out.println(str.toLowerCase());
+		
+//		검색 관련 메서드
+		System.out.println(str.charAt(5));	//	5번 인덱스의 char
+//		Java가 어느 인덱스에 있는가 ( * 중 요 )
+		int pos = str.indexOf("Java");	//	문자열 내부에서 Java의 위치
+		System.out.println("1st Search Java:" + pos);
+		pos = str.indexOf("Java", pos + 4);
+		System.out.println("2nd Search Java:" + pos);
+		pos = str.indexOf("Java", pos + 4);
+		System.out.println("3rd Search Java:" + pos);
+		
+//		검색할 부분문자열이 없으면 -1을 반환한다.
+		pos = str.lastIndexOf("Java");
+		System.out.println("Last Index Java:" + pos);
+		
+//		문자열 추출
+		System.out.println("SUBSTRING:" + str.substring(5));		
+		System.out.println("SUBSTRING:" + str.substring(5, 16));
+		
+//		문자열 치환
+		System.out.println("REPLACE:" + str.replace("Programming", "Coding"));
+		
+//		trimming : 화이트 스페이스 제거
+		String s6 = "                           Hello                                 ";
+		String s7 = ", Java";
+		
+		s6 = s6.trim();
+		s7 = s7.trim();
+		
+		System.out.println(s6 + s7);
+		
+//		문자열 분리 : split() : 특정 구분자를 기준으로 구분한다.
+		String[] tokens = str.replace("," , "").split(" ");
+		System.out.println("SPLIT:" + Arrays.toString(tokens));
+		
+//		문자열 비교 : Unicode
+//		compareTo ( *** 매 우 중 요 )
+//		-  결과값
+//		-> 0이면 순번이 같다.
+//		-> 값이 작으면 음수가 나온다.
+//		-> 값이 크면 양수가 나온다.
+		System.out.println("ABC".compareTo("ABD"));
 		
 	}
 	
